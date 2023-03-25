a528e889ab6dcabdf0c5cba0e16a6038e5c04d50
davidtinamart <dmarttin@myuax.com>
2023-03-26 00:10:33 +0100

    Commit 6 -> Clase RecursivoLambda -> Funcion recursiva e iterativa de factoresPrimos.

--- src/main/java/Ejercicio2/RecursivoLambda.java
+++ src/main/java/Ejercicio2/RecursivoLambda.java
@@ -14,6 +14,27 @@ public class RecursivoLambda {  //atributos independientes para cada metodo
             }
         }
     }
+    public static void factoresPrimosIterativo(int num){  //Función iterativa
+        int div = 2;                            //Divisor
+        while(num > 1){                        //Mientras el número sea mayor que 1
+            if(num % div == 0){              //Si el resto es 0
+                System.out.println(div);   //Se imprime el número
+                num /= div;             //Se divide el número entre el divisor (forma acortada de num = num / div)
+            }else{
+                div++;
+            }
+        }
+    }
+
+    public static void factoresPrimosLambda(int num){   //Función recursiva
+        IntConsumer f = n -> {                              //Función lambda
+            if(num % n == 0){                            //Si el resto es 0
+                System.out.println(n);                  //Se imprime el número
+                factoresPrimosLambda(num / n);   //Llamada recursiva
+            }
+        };
+        IntStream.rangeClosed(2, num).forEach(f);
+    }
 
 
 
