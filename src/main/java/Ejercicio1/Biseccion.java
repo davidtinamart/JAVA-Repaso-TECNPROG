acf3d468cf141fb025f193831b7157d38617fb1f
davidtinamart <dmarttin@myuax.com>
2023-03-26 00:03:01 +0100

    Commit 3 -> Clase Biseccion -> Metodo Iterativo

--- src/main/java/Ejercicio1/Biseccion.java
+++ src/main/java/Ejercicio1/Biseccion.java
@@ -19,4 +19,18 @@ public class Biseccion {
             return biseccionrecursiva(med, max);                            // Llamada recursiva
         }
     }
+
+    public static double biseccioniterativa(double min, double max){           // Bisección iterativa
+        double med = (min + max) / 2;                                           // Se calcula el punto medio
+        while(max-min >= PREC){                                                 // Mientras la diferencia entre los puntos sea mayor o igual a la precisión
+            if(f(min)*f(med) < 0){                                               // Si el producto es menor que 0, entonces hay cambio de signo
+                max = med;                                                      // Se actualiza el valor máximo
+            }else{                                                              // Si el producto es mayor o igual que 0, entonces no hay cambio de signo
+                min = med;                                                      // Se actualiza el valor mínimo
+            }
+            med = (min + max) / 2;                                              // Se calcula el punto medio
+
+        }
+        return med;                                                             // Se devuelve el punto medio
+    }
 }
