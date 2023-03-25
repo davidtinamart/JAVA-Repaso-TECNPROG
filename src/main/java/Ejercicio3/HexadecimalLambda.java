030d6795c0860d5b72b5efdca8417cb7dff263aa
davidtinamart <dmarttin@myuax.com>
2023-03-26 00:19:22 +0100

    Commit 9 -> Clase HexadecimalLambda -> Metodo main y fichero.class

--- src/main/java/Ejercicio3/HexadecimalLambda.java
+++ src/main/java/Ejercicio3/HexadecimalLambda.java
@@ -17,4 +17,13 @@ public class HexadecimalLambda {
         return resultado;
     }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número decimal: ");
+        int num = sc.nextInt();
+        System.out.println("Introduce la base: ");
+        int base = sc.nextInt();
+        System.out.println("El número " + num + " en base " + base + " es: " + convertir(num, base));
+    }
+
 }
